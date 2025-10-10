4e6cc27aef40ca84e526022627c8aef59511ad46
Justin Coyne <jcoyne@justincoyne.com>
2025-10-10 11:14:58 -0500

    Move the identifier methods into the Request object
    Move them out of the AbstractResource
    
    Ref #848

--- src/main/java/edu/illinois/library/cantaloupe/resource/AbstractResource.java
+++ src/main/java/edu/illinois/library/cantaloupe/resource/AbstractResource.java
@@ -45,8 +45,6 @@ import java.util.stream.Collectors;
  */
 public abstract class AbstractResource {
 
-    public static final String PUBLIC_IDENTIFIER_HEADER = "X-Forwarded-ID";
-
     static final String RESPONSE_CONTENT_DISPOSITION_QUERY_ARG =
             "response-content-disposition";
 
@@ -55,15 +53,11 @@ public abstract class AbstractResource {
      */
     private DelegateProxy delegateProxy;
 
-    private List<String> pathArguments          = Collections.emptyList();
     private final RequestContext requestContext = new RequestContext();
     private Request request;
     private HttpServletResponse response;
 
-    /**
-     * Cached by {@link #getIdentifier()}.
-     */
-    private Identifier identifier;
+
 
     /**
      * Cached by {@link #getMetaIdentifier()}.
@@ -413,47 +407,7 @@ public abstract class AbstractResource {
         return delegateProxy;
     }
 
-    /**
-     * <p>Returns the decoded identifier path component of the URI. (This may
-     * not be the identifier that the client supplies or sees; for that, use
-     * {@link #getPublicIdentifier()}.)</p>
-     *
-     * <p>N.B.: Depending on the image request endpoint API, The return value
-     * may include "meta-information" that is not part of the identifier but is
-     * encoded along with it. In that case, it is not safe to consume via this
-     * method, and {@link #getMetaIdentifier()} should be used instead.</p>
-     *
-     * @return Identifier, or {@code null} if the URI does not have an
-     *         identifier path component.
-     * @see #getMetaIdentifier()
-     * @see #getPublicIdentifier()
-     */
-    protected Identifier getIdentifier() {
-        if (identifier == null) {
-            String pathComponent = getIdentifierPathComponent();
-            if (pathComponent != null) {
-                identifier = Identifier.fromURIPathComponent(pathComponent);
-            }
-        }
-        return identifier;
-    }
 
-    /**
-     * <p>Returns the first {@link #getPathArguments() path argument}. (Most
-     * resources have an identifier as the first path argument, so this will
-     * work for them, but if not, an override will be necessary.)</p>
-     *
-     * <p>The result is not decoded and may be a {@link MetaIdentifier
-     * meta-identifier}. As such, it is not usable without additional
-     * processing.</p>
-     *
-     * @return Identifier, or {@code null} if no path arguments are
-     *         available.
-     */
-    protected String getIdentifierPathComponent() {
-        List<String> args = getPathArguments();
-        return (!args.isEmpty()) ? args.get(0) : null;
-    }
 
     abstract protected Logger getLogger();
 
@@ -463,15 +417,15 @@ public abstract class AbstractResource {
      * component that the client supplies or sees; for that, use {@link
      * #getPublicIdentifier()}.)
      *
-     * @return Instance corresponding to the first {@link #getPathArguments()
+     * @return Instance corresponding to the first {@link Request#getPathArguments()
      *         path argument}, or {@code null} if no path arguments are
      *         available.
-     * @see #getIdentifier()
-     * @see #getPublicIdentifier()
+     * @see Request#getIdentifier()
+     * @see Request#getPublicIdentifier()
      */
     protected MetaIdentifier getMetaIdentifier() {
         if (metaIdentifier == null) {
-            String pathComponent = getIdentifierPathComponent();
+            String pathComponent = getRequest().getIdentifierPathComponent();
             if (pathComponent != null) {
                 metaIdentifier = MetaIdentifier.fromURIPathComponent(
                         pathComponent, getDelegateProxy());
@@ -481,15 +435,6 @@ public abstract class AbstractResource {
         return metaIdentifier;
     }
 
-    /**
-     * Returns the segments of the URI path that are considered arguments.
-     * (These may correspond to regex match groups in {@link Route}.)
-     *
-     * @return Path arguments, or an empty list if there are none.
-     */
-    protected final List<String> getPathArguments() {
-        return pathArguments;
-    }
 
     /**
      * @return List of client-preferred media types as expressed in the
@@ -502,21 +447,6 @@ public abstract class AbstractResource {
         return negotiator.getPreferredMediaTypes();
     }
 
-    /**
-     * <p>Returns the identifier that the client sees. This will be the value
-     * of the {@link #PUBLIC_IDENTIFIER_HEADER} header, if available, or else
-     * the {@code identifier} URI path component.</p>
-     *
-     * <p>The result is not decoded, as the encoding may be influenced by
-     * {@link Key#SLASH_SUBSTITUTE}, for example.</p>
-     *
-     * @see #getIdentifier()
-     */
-    protected String getPublicIdentifier() {
-        return request.getHeaders().getFirstValue(
-                PUBLIC_IDENTIFIER_HEADER,
-                getIdentifierPathComponent());
-    }
 
     /**
      * <p>Returns the current public reference.</p>
@@ -558,7 +488,7 @@ public abstract class AbstractResource {
         final Reference publicRef         = new Reference(getPublicReference());
         final List<String> pathComponents = publicRef.getPathComponents();
         final int identifierIndex         = pathComponents.indexOf(
-                getIdentifierPathComponent());
+                getRequest().getIdentifierPathComponent());
 
         final String newMetaIdentifierString =
                 newMetaIdentifier.toURIPathComponent(getDelegateProxy());
@@ -659,10 +589,6 @@ public abstract class AbstractResource {
         return new Method[] { Method.OPTIONS };
     }
 
-
-    final void setPathArguments(List<String> pathArguments) {
-        this.pathArguments = pathArguments;
-    }
 
     /**
      * @param request Request being handled.
--- src/main/java/edu/illinois/library/cantaloupe/resource/iiif/v3/InformationResource.java
+++ src/main/java/edu/illinois/library/cantaloupe/resource/iiif/v3/InformationResource.java
@@ -136,7 +136,7 @@ public class InformationResource extends IIIF3Resource {
      */
     private String getImageURI() {
         return getPublicRootReference() + Route.IIIF_3_PATH + "/" +
-                getPublicIdentifier();
+                getRequest().getPublicIdentifier();
     }
 
     private String getNegotiatedContentType() {
