57550e296fd64e0637557a4ff5a243244ba1d58a
Justin Coyne <jcoyne@justincoyne.com>
2025-10-10 11:14:58 -0500

    Move the identifier methods into the Request object
    Move them out of the AbstractResource
    
    Ref #848

--- src/test/java/edu/illinois/library/cantaloupe/resource/AbstractResourceTest.java
+++ src/test/java/edu/illinois/library/cantaloupe/resource/AbstractResourceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -27,7 +28,7 @@ public class AbstractResourceTest extends BaseTest {
             }
         };
 
-        Request mockRequest = new Request(new MockHttpServletRequest());
+        Request mockRequest = new Request(new MockHttpServletRequest(), Collections.emptyList());
         instance.setRequest(mockRequest);
         instance.setResponse(new MockHttpServletResponse());
     }
