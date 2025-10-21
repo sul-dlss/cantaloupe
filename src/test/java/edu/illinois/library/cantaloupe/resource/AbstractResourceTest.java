e363ff9f56dc9489e1be4d7c81c32ecd40a09bc6
Justin Coyne <jcoyne@justincoyne.com>
2025-10-21 15:40:09 -0500

    Remove unused imports

--- src/test/java/edu/illinois/library/cantaloupe/resource/AbstractResourceTest.java
+++ src/test/java/edu/illinois/library/cantaloupe/resource/AbstractResourceTest.java
@@ -1,6 +1,5 @@
 package edu.illinois.library.cantaloupe.resource;
 
-import edu.illinois.library.cantaloupe.image.Format;
 import edu.illinois.library.cantaloupe.test.BaseTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,7 +8,6 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
