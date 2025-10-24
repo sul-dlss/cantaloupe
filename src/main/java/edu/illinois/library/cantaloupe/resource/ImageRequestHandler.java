d9df45fb861fdaf2530458bdb49973fb1c6decab
Justin Coyne <jcoyne@justincoyne.com>
2025-10-23 23:26:38 -0500

    Convert Version3_0ConformanceTest to use MockMvc

--- src/main/java/edu/illinois/library/cantaloupe/resource/ImageRequestHandler.java
+++ src/main/java/edu/illinois/library/cantaloupe/resource/ImageRequestHandler.java
@@ -128,6 +128,7 @@ public class ImageRequestHandler extends AbstractRequestHandler
     private Callback callback;
     private OperationList operationList;
     private Future<Path> tempFileFuture;
+    private Configuration configuration;
 
 
     /**
@@ -161,7 +162,7 @@ public class ImageRequestHandler extends AbstractRequestHandler
         this.callback = callback;
         this.isBypassingCache = request.isBypassingCache();
         this.isBypassingCacheRead = request.isBypassingCacheRead();
-        // Store configuration for potential future use
+        this.configuration = configuration;
     }
 
     /**
@@ -201,7 +202,6 @@ public class ImageRequestHandler extends AbstractRequestHandler
         }
 
         final Identifier identifier   = operationList.getIdentifier();
-        final Configuration config    = Configuration.getInstance();
         final CacheFacade cacheFacade = new CacheFacade();
 
         Iterator<Format> formatIterator = Collections.emptyIterator();
@@ -242,7 +242,7 @@ public class ImageRequestHandler extends AbstractRequestHandler
             }
         }
 
-        final Source source = new SourceFactory(config).newSource(
+        final Source source = new SourceFactory(configuration).newSource(
                 identifier, delegateProxy);
 
         // If we are resolving first, or if the source image is not present in
@@ -254,7 +254,7 @@ public class ImageRequestHandler extends AbstractRequestHandler
                 StatResult result = source.stat();
                 callback.sourceAccessed(result);
             } catch (NoSuchFileException e) { // this needs to be rethrown!
-                if (config.getBoolean(Key.CACHE_SERVER_PURGE_MISSING, false)) {
+                if (configuration.getBoolean(Key.CACHE_SERVER_PURGE_MISSING, false)) {
                     // If the image was not found, purge it from the cache.
                     cacheFacade.purgeAsync(operationList.getIdentifier());
                 }
@@ -333,7 +333,7 @@ public class ImageRequestHandler extends AbstractRequestHandler
                         format, identifier);
             }
         }
-        if (config.getBoolean(Key.PROCESSOR_PURGE_INCOMPATIBLE_FROM_SOURCE_CACHE, false)) {
+        if (configuration.getBoolean(Key.PROCESSOR_PURGE_INCOMPATIBLE_FROM_SOURCE_CACHE, false)) {
             TaskQueue.getInstance().submit(() -> {
                 try {
                     cacheFacade.getSourceCacheFile(identifier).ifPresent(file -> {
